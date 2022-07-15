fcebab259bc8fdb12c0fcfae1f3c61e0874b8ada
abrahamrivv <abrahamrivv@gmail.com>
2022-07-15 14:13:20 -0500

    Variables de estado y globales

---
+++ ProyectCrowdfunding/VariablesEstadoYGlobales.sol
@@ -0,0 +1,17 @@
+//"SPDX-License-Identifier: GPL-3.0
+
+pragma solidity >=0.7.0 <0.9.0;
+
+contract Identity {
+    uint public idNumber;
+    bool public isWorking;
+    string public name;
+    address public wallet;
+
+    constructor(uint _idNumber, bool _isWorking, string memory _name){
+        idNumber = _idNumber;
+        isWorking = _isWorking;
+        name = _name;
+        wallet = msg.sender;
+    }
+}
