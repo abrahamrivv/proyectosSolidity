80071235595820c20fe0a561966f1bf399a0bc62
abrahamrivv <abrahamrivv@gmail.com>
2022-08-05 16:59:02 -0500

    estructura de un contrato

---
+++ IntroduccionSolidity/EstructuraContracto.sol
@@ -0,0 +1,12 @@
+//"SPDX-License-Identifier: GPL-3.0
+// Version puede representar un rango. Ej >=0.7.0 <9.0.0
+pragma solidity <version>;
+
+// Declaración de un contrato, similar a una clase
+contract <name> { // Name puede ser cualquier cosa
+  
+  // Función de inicialización de un contrato
+  constructor() {
+    // Código de inicialización
+  }
+}
